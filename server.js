8404621cf760cb7353c0fa4462660cb78ab00e26
Kurt Miller <kmiller9393@gmail.com>
2018-10-04 11:38:24 -0600

    Update environment within server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
