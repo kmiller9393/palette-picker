00c4ea2b1fc7df9b58064e54f66636bc15514c19
Kurt Miller <kmiller9393@gmail.com>
2018-10-06 12:47:45 -0600

    Add fetch functionality to get projects and its corresponding palettes

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,66 @@
 const randomizeButton = $('.radomize-palette');
 const lockButton = $('.padlock-image');
+const projectContainer = $('.project-container');
 
-$(document).ready(() => setNewColor());
+const getProjects = async () => {
+  const url = '/api/v1/projects';
+
+  const response = await fetch(url);
+  const projects = await response.json();
+  const palettes = await getPalettes();
+  console.log(projects);
+  console.log(palettes);
+
+  const colors = {
+    colors: []
+  };
+
+  const allColors = palettes.map(palette => {
+    return [
+      palette.color_1,
+      palette.color_2,
+      palette.color_3,
+      palette.color_4,
+      palette.color_5
+    ];
+  });
+
+  const allProjects = projects.map(
+    project =>
+      console.log(project) ||
+      projectContainer.append(
+        `<section class="saved-palette-container">${project.project_name
+          .charAt(0)
+          .toUpperCase() + project.project_name.slice(1)}: 
+        <div class="single-palette" style="background-color:${
+          allColors[0][0]
+        }"></div>
+        <div class="single-palette" style="background-color:${
+          allColors[0][1]
+        }"></div>
+        <div class="single-palette" style="background-color:${
+          allColors[0][2]
+        }"></div>
+          <div class="single-palette" style="background-color:${
+            allColors[0][3]
+          }"></div>
+          <div class="single-palette" style="background-color:${
+            allColors[0][4]
+          }"></div>
+      </section>`
+      )
+  );
+};
+
+const getPalettes = async () => {
+  const url = '/api/v1/palettes';
+
+  const response = await fetch(url);
+  const palettes = await response.json();
+  return palettes;
+};
+
+$(document).ready(() => setNewColor(), getProjects());
 
 window.onkeydown = e => {
   if (e.keyCode === 32) setNewColor();
